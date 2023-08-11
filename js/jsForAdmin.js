6d4634ceebe37f5ab9f5545ce9e13f778b79a547
subhamdas04 <subhamdas0465@gmail.com>
2023-08-11 17:05:17 +0530

    js uploaded

---
+++ js/jsForAdmin.js
@@ -0,0 +1,7 @@
+function hideConfirmBtn() {
+    document.getElementById("logoutconfirm").style.display = "none";
+}
+
+function showConfirmBtn() {
+    document.getElementById("logoutconfirm").style.display = "flex";
+}
---
+++ js/jsForArticles.js
@@ -0,0 +1,20 @@
+// buying button flex
+function buyNowOverDivdisplay() {
+    document.getElementById("buyNowOverDiv").style.display = "flex";
+}
+function buyNowOverDivHide() {
+    document.getElementById("buyNowOverDiv").style.display = "none";
+}
+
+
+
+// showing sample-img in the sample box
+function displayImg(e) {
+    document.getElementsByClassName("samples-showBox")[0].style.backgroundImage="url(" + e.src + ")";
+    document.getElementsByClassName("samples-showBox")[0].style.backgroundSize="100% 100%";
+}
+
+$(document).ready(function(){
+    document.getElementsByClassName("samples-showBox")[0].style.backgroundImage="url(../req/1.jpg)";
+    document.getElementsByClassName("samples-showBox")[0].style.backgroundSize="100% 100%";
+});
