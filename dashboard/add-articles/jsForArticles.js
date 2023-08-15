7e97e5615808b7050ecbc282679affbd119c4b2c
subhamdas04 <subhamdas0465@gmail.com>
2023-08-16 01:22:59 +0530

    next div java

---
+++ dashboard/add-articles/jsForArticles.js
@@ -0,0 +1,118 @@
+// on basic for camera sample upload
+function showNextDiv1() {
+    document.getElementsByClassName("entryDivs")[0].style.display = "none";
+    document.getElementsByClassName("entryDivs")[1].style.display = "flex";
+}
+
+// on camera sample upload for Key Specifications
+function showNextDiv2() {
+    document.getElementsByClassName("entryDivs")[1].style.display = "none";
+    document.getElementsByClassName("entryDivs")[2].style.display = "flex";
+}
+
+// on Key Specifications for ProsAndCons
+function showNextDiv3() {
+    document.getElementsByClassName("entryDivs")[2].style.display = "none";
+    document.getElementsByClassName("entryDivs")[3].style.display = "flex";
+}
+
+// on ProsAndCons for Verdict
+function showNextDiv4() {
+    document.getElementsByClassName("entryDivs")[3].style.display = "none";
+    document.getElementsByClassName("entryDivs")[4].style.display = "flex";
+}
+
+// on Verdict for Detailed Specifications
+function showNextDiv5() {
+    document.getElementsByClassName("entryDivs")[4].style.display = "none";
+    document.getElementsByClassName("entryDivs")[5].style.display = "flex";
+}
+
+// on Detailed Specifications for Performances
+function showNextDiv6() {
+    document.getElementsByClassName("entryDivs")[5].style.display = "none";
+    document.getElementsByClassName("entryDivs")[6].style.display = "flex";
+}
+
+// on performance for design and sizes
+function showNextDiv7() {
+    document.getElementsByClassName("entryDivs")[6].style.display = "none";
+    document.getElementsByClassName("entryDivs")[7].style.display = "flex";
+}
+
+// on Design And Sizes for Display
+function showNextDiv8() {
+    document.getElementsByClassName("entryDivs")[7].style.display = "none";
+    document.getElementsByClassName("entryDivs")[8].style.display = "flex";
+}
+
+// on display for camera setup
+function showNextDiv9() {
+    document.getElementsByClassName("entryDivs")[8].style.display = "none";
+    document.getElementsByClassName("entryDivs")[9].style.display = "flex";
+}
+
+// on camera setup for battery
+function showNextDiv10() {
+    document.getElementsByClassName("entryDivs")[9].style.display = "none";
+    document.getElementsByClassName("entryDivs")[10].style.display = "flex";
+}
+
+// on battery for storage
+function showNextDiv11() {
+    document.getElementsByClassName("entryDivs")[10].style.display = "none";
+    document.getElementsByClassName("entryDivs")[11].style.display = "flex";
+}
+
+// on storage for Connectivity
+function showNextDiv12() {
+    document.getElementsByClassName("entryDivs")[11].style.display = "none";
+    document.getElementsByClassName("entryDivs")[12].style.display = "flex";
+}
+
+// on Connectivity for Sound
+function showNextDiv13() {
+    document.getElementsByClassName("entryDivs")[12].style.display = "none";
+    document.getElementsByClassName("entryDivs")[13].style.display = "flex";
+}
+
+// on Sound for Sensors
+function showNextDiv14() {
+    document.getElementsByClassName("entryDivs")[13].style.display = "none";
+    document.getElementsByClassName("entryDivs")[14].style.display = "flex";
+}
+
+// on Sensors for Antutu
+function showNextDiv15() {
+    document.getElementsByClassName("entryDivs")[14].style.display = "none";
+    document.getElementsByClassName("entryDivs")[15].style.display = "flex";
+}
+
+// on Antutu for Geekbench
+function showNextDiv16() {
+    document.getElementsByClassName("entryDivs")[15].style.display = "none";
+    document.getElementsByClassName("entryDivs")[16].style.display = "flex";
+}
+
+// on Geekbench for #affiliate
+function showNextDiv17() {
+    document.getElementsByClassName("entryDivs")[16].style.display = "none";
+    document.getElementsByClassName("entryDivs")[17].style.display = "flex";
+}
+
+// on #affiliate for Conclusions
+function showNextDiv18() {
+    document.getElementsByClassName("entryDivs")[17].style.display = "none";
+    document.getElementsByClassName("entryDivs")[18].style.display = "flex";
+}
+
+// on Conclusions for submit
+function showNextDiv19() {
+    document.getElementsByClassName("entryDivs")[18].style.display = "none";
+    document.getElementsByClassName("entryDivs")[19].style.display = "flex";
+}
+
+function SubmitPost() {
+
+}
+
