70b1e179620cfdbaac73c61498fe90a94a7b328b
subhamdas04 <subhamdas0465@gmail.com>
2023-08-16 02:39:07 +0530

    v

--- dashboard/add-articles/jsForArticles.js
+++ dashboard/add-articles/jsForArticles.js
@@ -64,55 +64,188 @@ function showNextDiv11() {
     document.getElementsByClassName("entryDivs")[11].style.display = "flex";
 }
 
-// on storage for Connectivity
+// on storage for Software
 function showNextDiv12() {
     document.getElementsByClassName("entryDivs")[11].style.display = "none";
     document.getElementsByClassName("entryDivs")[12].style.display = "flex";
 }
 
-// on Connectivity for Sound
+// on Software for Connectivity
 function showNextDiv13() {
     document.getElementsByClassName("entryDivs")[12].style.display = "none";
     document.getElementsByClassName("entryDivs")[13].style.display = "flex";
 }
 
-// on Sound for Sensors
+// on Connectivity for Sound
 function showNextDiv14() {
     document.getElementsByClassName("entryDivs")[13].style.display = "none";
     document.getElementsByClassName("entryDivs")[14].style.display = "flex";
 }
 
-// on Sensors for Antutu
+// on Sound for Sensors
 function showNextDiv15() {
     document.getElementsByClassName("entryDivs")[14].style.display = "none";
     document.getElementsByClassName("entryDivs")[15].style.display = "flex";
 }
 
-// on Antutu for Geekbench
+// on Sensors for Antutu
 function showNextDiv16() {
     document.getElementsByClassName("entryDivs")[15].style.display = "none";
     document.getElementsByClassName("entryDivs")[16].style.display = "flex";
 }
 
-// on Geekbench for #affiliate
+// on Antutu for #Geekbench
 function showNextDiv17() {
     document.getElementsByClassName("entryDivs")[16].style.display = "none";
     document.getElementsByClassName("entryDivs")[17].style.display = "flex";
 }
 
-// on #affiliate for Conclusions
+// on #Geekbench for affiliate
 function showNextDiv18() {
     document.getElementsByClassName("entryDivs")[17].style.display = "none";
     document.getElementsByClassName("entryDivs")[18].style.display = "flex";
 }
 
-// on Conclusions for submit
+// on affiliate for Conclusions
 function showNextDiv19() {
     document.getElementsByClassName("entryDivs")[18].style.display = "none";
     document.getElementsByClassName("entryDivs")[19].style.display = "flex";
 }
 
+
+// above codes are for next div only
+
+
+
+
+
+
+
+// show Previous div
+
+// on camera sample upload for basic  
+function showPrevdiv2(){
+    document.getElementsByClassName("entryDivs")[0].style.display = "flex";
+    document.getElementsByClassName("entryDivs")[1].style.display = "none";
+}
+
+// on KeySpecifications for CameraSamples  
+function showPrevdiv3(){
+    document.getElementsByClassName("entryDivs")[1].style.display = "flex";
+    document.getElementsByClassName("entryDivs")[2].style.display = "none";
+}
+
+// on ProsAndCons for KeySpecifications 
+function showPrevdiv4(){
+    document.getElementsByClassName("entryDivs")[2].style.display = "flex";
+    document.getElementsByClassName("entryDivs")[3].style.display = "none";
+}
+
+// on Verdict for ProsAndCons 
+function showPrevdiv5(){
+    document.getElementsByClassName("entryDivs")[3].style.display = "flex";
+    document.getElementsByClassName("entryDivs")[4].style.display = "none";
+}
+
+// on Detailed Specifications for Verdict 
+function showPrevdiv6(){
+    document.getElementsByClassName("entryDivs")[4].style.display = "flex";
+    document.getElementsByClassName("entryDivs")[5].style.display = "none";
+}
+
+// on Performances for Detailed Specifications 
+function showPrevdiv7(){
+    document.getElementsByClassName("entryDivs")[5].style.display = "flex";
+    document.getElementsByClassName("entryDivs")[6].style.display = "none";
+}
+
+// on DesignAndSizes for Performances 
+function showPrevdiv8(){
+    document.getElementsByClassName("entryDivs")[6].style.display = "flex";
+    document.getElementsByClassName("entryDivs")[7].style.display = "none";
+}
+
+// on Display for DesignAndSizes 
+function showPrevdiv9(){
+    document.getElementsByClassName("entryDivs")[7].style.display = "flex";
+    document.getElementsByClassName("entryDivs")[8].style.display = "none";
+}
+
+// on CameraSetup for Display 
+function showPrevdiv10(){
+    document.getElementsByClassName("entryDivs")[8].style.display = "flex";
+    document.getElementsByClassName("entryDivs")[9].style.display = "none";
+}
+
+// on Battery for CameraSetup 
+function showPrevdiv11(){
+    document.getElementsByClassName("entryDivs")[9].style.display = "flex";
+    document.getElementsByClassName("entryDivs")[10].style.display = "none";
+}
+
+// on Storage for Battery 
+function showPrevdiv12(){
+    document.getElementsByClassName("entryDivs")[10].style.display = "flex";
+    document.getElementsByClassName("entryDivs")[11].style.display = "none";
+}
+
+// on Software for Storage 
+function showPrevdiv13(){
+    document.getElementsByClassName("entryDivs")[11].style.display = "flex";
+    document.getElementsByClassName("entryDivs")[12].style.display = "none";
+}
+
+// on Connectivity for Software 
+function showPrevdiv14(){
+    document.getElementsByClassName("entryDivs")[12].style.display = "flex";
+    document.getElementsByClassName("entryDivs")[13].style.display = "none";
+}
+
+// on Sound for Connectivity 
+function showPrevdiv15(){
+    document.getElementsByClassName("entryDivs")[13].style.display = "flex";
+    document.getElementsByClassName("entryDivs")[14].style.display = "none";
+}
+
+// on Sensors for Sound 
+function showPrevdiv16(){
+    document.getElementsByClassName("entryDivs")[14].style.display = "flex";
+    document.getElementsByClassName("entryDivs")[15].style.display = "none";
+}
+
+// on Antutu for Sensors 
+function showPrevdiv17(){
+    document.getElementsByClassName("entryDivs")[15].style.display = "flex";
+    document.getElementsByClassName("entryDivs")[16].style.display = "none";
+}
+
+// on Geekbench for Antutu 
+function showPrevdiv18(){
+    document.getElementsByClassName("entryDivs")[16].style.display = "flex";
+    document.getElementsByClassName("entryDivs")[17].style.display = "none";
+}
+
+// on affiliate for Geekbench 
+function showPrevdiv19(){
+    document.getElementsByClassName("entryDivs")[17].style.display = "flex";
+    document.getElementsByClassName("entryDivs")[18].style.display = "none";
+}
+
+// on Conclusions for affiliate 
+function showPrevdiv20(){
+    document.getElementsByClassName("entryDivs")[18].style.display = "flex";
+    document.getElementsByClassName("entryDivs")[19].style.display = "none";
+}
+
+
+
+
+
+
+
+
+
+// for submission
 function SubmitPost() {
 
 }
-
